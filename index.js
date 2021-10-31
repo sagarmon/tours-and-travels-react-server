661e7eb5233d5f8d1642ab9408e2edaaa10d385e
Sagar Sarkar <sagar_mon@hotmail.com>
2021-10-31 16:51:46 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bosfk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('connected to database');
+
+        //// remove start
+        const database = client.db('assignment11db');
+        const servicesCollection = database.collection('services');
+
+        // GET API
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+        //// remove end
+
+        // GET Single Service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('getting specific service', id);
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Running Assignment 11');
+});
+
+app.listen(port, () => {
+    console.log('Running Assignment 11 on port', port);
+});
